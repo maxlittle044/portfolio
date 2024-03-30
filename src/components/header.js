6d9014a0a49e1a75731af798f78cf60959ec7549
Gajendra Sah <sahgajendra121@gmail.com>
2024-03-30 16:46:37 +0545

    hamburger menu styling updates

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useEffect } from 'react'
 import Nav from './nav';
 import { Context } from "../redux/store";
 import { Link } from 'gatsby';
 
 function Header() {
     const { state, dispatch } = useContext(Context);
+    const headerRef = useRef(null);
 
     const handleHamburgerClick = () => {
         dispatch({ type: 'SET_MOBILE_MENU', payload: !state.hamburgerOpen })
     }
 
+    useEffect(() => {
+        if (state.hamburgerOpen) {
+            const headerHeight = headerRef.current.offsetHeight;
+            document.getElementById('nav').style.paddingTop = `${headerHeight}px`;
+        } else {
+            document.getElementById('nav').style.paddingTop = '0px';
+        }
+    }, [state.hamburgerOpen]);
+
     return (
-        <header className={`site-header ${state.hamburgerOpen ? 'show-navbar' : ''}`}>
+        <header className={`site-header ${state.hamburgerOpen ? 'show-navbar' : ''}`} ref={headerRef}>
             <div className="container">
                 <div className="site-header__content relative z-[999] flex flex-wrap items-center justify-between lg:hidden">
                     <div className="site-header__logo">
@@ -20,14 +30,23 @@ function Header() {
                         </Link>
                     </div>
                     <div className="site-header__menu">
-                        <button onClick={handleHamburgerClick} className={`flex items-center px-3 py-2 text-indigo-500 border border-indigo-500 rounded navbar-burger ${state.hamburgerOpen ? 'menu-active' : ''}`}>
-                            <svg className="w-3 h-3 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <title>
-                                    Menu
-                                </title>
-                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z">
-                                </path>
+                        <button
+                            onClick={handleHamburgerClick}
+                            className={`flex items-center p-1 rounded-full border-2 border-t-black border-r-black border-b-black border-l-black menu navbar-menu transition-all duration-500
+                            ${state.hamburgerOpen ? 'active border-t-white border-r-white border-b-white border-l-white' : ''}`}
+                            aria-label="Main Menu"
+                        >
+                            {/* <svg width="100" height="100" viewBox="0 0 100 100">
+                                <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+                                <path class="line line2" d="M 20,50 H 80" />
+                                <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+                            </svg> */}
+                            <svg width="50" height="50" viewBox="0 0 50 50">
+                                <path className="line line1" d="M 10,14.5 H 40 C 40,14.5 47.2494195,14.408676 47.2664935,33.3556655 47.271571,38.9903365 45.4830405,40.835123 42.6295865,40.833499 39.7761345,40.831875 37.5001055,37.499971 37.5001055,37.499971 L 12.5000105,12.500029" />
+                                <path className="line line2" d="M 10,25 H 40" />
+                                <path className="line line3" d="M 10,35.499977 H 40 C 40,35.499977 47.2494195,35.591324 47.2664935,16.6443355 47.271571,10.0096645 45.4830405,8.164877 42.6295865,8.1665005 39.7761345,8.1681245 37.5001055,11.500029 37.5001055,11.500029 L 12.5000105,37.499971" />
                             </svg>
+
                         </button>
                     </div>
                 </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { useLocation } from '@reach/router';
 import { Context } from "../redux/store";
 import { Link } from 'gatsby';
+import CardSocial from './card/card-social';
 
 function Nav() {
   const location = useLocation()
@@ -16,7 +17,7 @@ function Nav() {
   }, [location.href])
 
   return (
-    <nav className={`[transition:all_.3s] w-full lg:relative lg:flex lg:flex-wrap lg:items-center lg:justify-between navbar-nav ${state.hamburgerOpen ? 'active' : ''}`}>
+    <nav id="nav" className={`[transition:all_.3s] w-full lg:relative lg:flex lg:flex-wrap lg:items-center lg:justify-between navbar-nav ${state.hamburgerOpen ? 'active' : ''}`}>
       <div className="w-auto lg:order-2 lg:w-1/5 lg:text-center">
         <div className="hidden lg:block">
           <Link to='/' className="text-xl font-semibold text-gray-800 font-heading">
@@ -26,27 +27,37 @@ function Nav() {
         {/* <span className='block lg:inline-block mt-4 lg:mt-0 mr-10 relative z-0 before:absolute before:left-0 before:top-0 before:opacity-0 before:w-0 before:h-[2px] before:bg-gradient-to-r before:from-yellow-500 before:to-red-500 before:transition-all before:duration-500 after:absolute after:right-0 after:bottom-0 after:opacity-0 after:w-[0%] after:h-[2px] after:bg-gradient-to-l after:from-yellow-500 after:to-red-500 after:transition-all after:duration-500 hover:before:w-full hover:before:opacity-100 hover:after:w-full hover:after:opacity-100 gradient-text gradient-text--purple'></span> */}
       </div>
 
-      <div className={`w-full navbar-menu lg:order-1 lg:block lg:w-2/5 `}>
-        <Link className="block mt-4 mr-10 text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="/">
+      <div className={`px-[1rem] w-full sm:max-w-screen-sm md:max-w-screen-md sm:mx-auto lg:max-w-none navbar-menu lg:order-1 lg:px-0 lg:block lg:w-2/5 `}>
+        <Link className="block mt-4 mr-10 text-white lg:text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="/">
           Home
         </Link>
-        <Link className="block mt-4 mr-10 text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
+        <Link className="block mt-4 mr-10 text-white lg:text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
           Team
         </Link>
-        <Link className="block mt-4 text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
+        <Link className="block mt-4 text-white lg:text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
           Galery
         </Link>
       </div>
-      <div className={`w-full navbar-menu lg:order-3 lg:block lg:w-2/5 lg:text-right `}>
-        <Link className="block mt-4 mr-10 text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
+      <div className={`px-[1rem] w-full sm:max-w-screen-sm md:max-w-screen-md sm:mx-auto lg:max-w-none navbar-menu lg:order-3 lg:px-0 lg:block lg:w-2/5 lg:text-right `}>
+        <Link className="block mt-4 mr-10 text-white lg:text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
           Content
         </Link>
-        <Link className="block mt-4 mr-10 text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
+        <Link className="block mt-4 mr-10 text-white lg:text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
           FAQ
         </Link>
-        <Link className="block mt-4 text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
+        <Link className="block mt-4 text-white lg:text-blue-900 lg:inline-block lg:mt-0 hover:text-indigo-600" to="#">
           Contact
         </Link>
+      </div>
+      <div className={`mt-auto pb-8 px-[1rem] w-full navbar-menu lg:hidden `}>
+        <div className="pt-8 flex max-w-xs mx-auto items-center justify-center">
+          <CardSocial
+            facebook
+            linkedIn
+            twitter
+            Instagram
+          />
+        </div>
       </div>
     </nav>
   );
